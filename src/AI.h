41677d3f7585b9957c754c354b0d41bd9128019f
Hayato Nishioka <tapioka.euler@gmail.com>
2016-11-21 03:00:09 +0900

    copied files from robot ai

---
+++ src/AI.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "Const.h"
+#include "Operation.h"
+#include "RobotData.h"
+#include "Position.h"
+#include "AIParams.h"
+
+enum ERoute {
+	DIRECT,
+	DODGE,
+};
+
+enum EStrategy {
+	RANDOM,
+	SIMPLE,
+};
+
+class AI {
+public:
+	void init(int _myid);
+	void setStrategy(EStrategy _estr);
+	void setRoute(ERoute _erou);
+
+	// äÓíÍÉNÉâÉXÇ≈íäèoÇ∑ÇÈÇ©Ç‡
+	void update();
+	void setRobotData(int _id, RobotData _data);
+	void setPOOwner(int _id, ETeam _owner);
+	Operation getOperation();
+
+	void developRandomRoute();
+	void developDirectRoute();
+	void developDodgeRoute();
+
+	void developSimpleStrategy();
+
+protected:
+	Operation operation;
+	RobotData data[NUM_OF_ROBOT];
+	ETeam owner[NUM_OF_POINT_OBJ];
+
+private:
+	int Myid;
+	ERoute erou;
+	EStrategy estr;
+	Position target;
+	Position POPos[NUM_OF_POINT_OBJ];
+};
